56d05c7f67fbad1910f638ae193e4bb6cc4e9a49
Amit Kalal <94915081+Amitk249@users.noreply.github.com>
2024-08-05 05:39:11 +0530

    Morse code converter web app

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 
+    'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', 
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', 
+    '9': '----.', '0': '-----', ',': '--..--', '.': '.-.-.-', '?': '..--..', 
+    "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', 
+    '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', 
+    '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.', 
+    ' ': '/'
+}
+
+def string_to_morse(input_string):
+    """Convert a string to Morse code."""
+    morse_code = []
+    for char in input_string.upper():
+        if char in MORSE_CODE_DICT:
+            morse_code.append(MORSE_CODE_DICT[char])
+        else:
+            morse_code.append('')  # for any character not in the dictionary
+    return ' '.join(morse_code)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    data = request.json
+    input_string = data.get('text', '')
+    morse_code = string_to_morse(input_string)
+    return jsonify({'morse_code': morse_code})
+
+if __name__ == '__main__':
+    app.run(debug=True)
